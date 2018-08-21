04568ecc4ae2fb7d337b90b86dfe05ef1858ebfc
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-08-21 15:45:55 -0500

    fixed call to functions imported

--- R/BMTMERS.R
+++ R/BMTMERS.R
@@ -5,7 +5,7 @@
 #' @param nIter number of iterations
 #' @param burnIn number of burning
 #' @param thin number of thinning
-#' @param CrossValidation cv object
+#' @param testingSet cv object
 #' @param digits number of digits of accuracy in the results
 #'
 #' @return Something cool
@@ -15,12 +15,12 @@
 #' @importFrom BGLR BGLR
 #' @examples
 #' ETA=list(Env=list(X=Z.E,model="BRR"),Gen = list(X = Z.G, model = 'BRR'), EnvGen=list(X=Z.EG,model="BRR"))
-#' CrossValidation <- BMTME::CV.RandomPart(pheno, NPartitions = 10, PTesting = 0.2, set_seed = 123)
+#' testingSet <- BMTME::CV.RandomPart(pheno, NPartitions = 10, PTesting = 0.2, set_seed = 123)
 BMTMERS <- function(Y = NULL, ETA = NULL, nIter = 2500, burnIn = 500,
-                 thin = 5, progressBar = TRUE, CrossValidation = NULL, digits = 4){
+                 thin = 5, progressBar = TRUE, testingSet = NULL, digits = 4){
   time.init <- proc.time()[3]
   YwithCov <- Y # to include covariance data
-  nCV <- length(CrossValidation$CrossValidation_list) #Number of cross-validations
+  nCV <- length(testingSet$CrossValidation_list) #Number of cross-validations
   Yhat_post <- matrix(NA, ncol = nCV, nrow = nrow(Y)) # save predictions
   nTraits <- dim(Y)[2L]
   results <- data.frame() # save cross-validation results
@@ -34,13 +34,13 @@ BMTMERS <- function(Y = NULL, ETA = NULL, nIter = 2500, burnIn = 500,
         pb$tick(tokens = list(what = paste0('Estimating covariance of trait ', colnames(Y)[t], ' in CV ', actual_CV, ' out of ', nCV)))
       }
       y1 <- y2
-      positionTST <- CrossValidation$CrossValidation_list[[actual_CV]]
+      positionTST <- testingSet$CrossValidation_list[[actual_CV]]
       y1[positionTST] <- NA
 
       fm <- BGLR(y1, ETA = ETA, nIter = nIter, burnIn = burnIn, thin = thin, verbose = F)
-      Yhat_post[, actual_CV] <- fm$predictions
+      Yhat_post[, actual_CV] <- fm$yHat
     }
-    cleanDat(noConfirm = T)
+
     YwithCov[, nTraits + t] <- apply(Yhat_post, 1L, mean)
   }
 
@@ -55,17 +55,17 @@ BMTMERS <- function(Y = NULL, ETA = NULL, nIter = 2500, burnIn = 500,
         pb$tick(tokens = list(what = paste0('Fitting Cross-Validation of trait ', colnames(Y)[t], ' in CV ', actual_CV, ' out of ', nCV)))
       }
       y1 <- y2
-      positionTST <- CrossValidation$CrossValidation_list[[actual_CV]]
+      positionTST <- testingSet$CrossValidation_list[[actual_CV]]
       y1[positionTST] <- NA
 
-      fm1 <- BGLR(y1, ETA = ETA1, nIter = nIter, burnIn = burnIn, thin = thin, verbose = F)
+      fm <- BGLR(y1, ETA = ETA1, nIter = nIter, burnIn = burnIn, thin = thin, verbose = F)
 
       results <- rbind(results, data.frame(Position = positionTST,
-                                           Environment = matrixData$Env[positionTST],
+                                           Environment = testingSet$Environments[positionTST],
                                            Trait = colnames(Y)[t],
                                            Partition = actual_CV,
                                            Observed = round(y2[positionTST], digits), #$response, digits),
-                                           Predicted = round(fm1$predictions[positionTST], digits)))
+                                           Predicted = round(fm$yHat[positionTST], digits)))
     }
   }
   out <- list(results = results, covariance = Yhat_post, nIter = nIter, burnIn = burnIn, thin = thin, executionTime = proc.time()[3] - time.init)
