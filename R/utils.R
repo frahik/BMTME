3e30d97edb7da2563b1b54275ab51f2b97bc03a7
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-08-21 15:25:58 -0500

    new generic functions was added
    summary for BMTMERSCV object
    print for BMTMECV object
    print for BMECV object
    print for BMTMERSCV object
    plot for BMTMERSCV object
    boxplot for BMTMERSCV object

--- R/utils.R
+++ R/utils.R
@@ -87,9 +87,49 @@ summary.BMECV <- function(object, information = 'compact', digits = 4, ...){
 }
 
 
-# summary.BMTME <- function(object,...){
-#   if (!inherits(object, "BMTME")) Stop("This function only works for objects of class 'BMTME'")
-# }
+#' @title Summary.BMTMERSCV
+#'
+#' @description Solo es una prueba
+#'
+#' @param object \code{BMTMERSCV object} Objeto BMTMERSCV, resultado de ejecutar MTME()
+#' @param ... Further arguments passed to or from other methods.
+#' @param information compact, extended, complete
+#'
+#' @importFrom stats cor
+#' @importFrom dplyr summarise group_by select '%>%' mutate_if funs
+#'
+#' @export
+summary.BMTMERSCV <- function(object, information = 'compact', digits = 4, ...){
+  if (!inherits(object, "BMTMERSCV")) Stop("This function only works for objects of class 'BMTMERSCV'")
+
+  object$results %>%
+    group_by(Environment, Trait, Partition) %>%
+    summarise(Pearson = cor(Predicted, Observed, use = 'pairwise.complete.obs'),
+              MSEP = mean((Predicted - Observed)^2, na.rm = T)) %>%
+    select(Environment, Trait, Partition, Pearson, MSEP) %>%
+    mutate_if(is.numeric, funs(round(., digits))) %>%
+    as.data.frame() -> presum
+
+  presum %>%  group_by(Environment, Trait) %>%
+    summarise(SE_MSEP = sd(MSEP, na.rm = T)/sqrt(n()), MSEP = mean(MSEP, na.rm = T),
+              SE_Pearson = sd(Pearson, na.rm = T)/sqrt(n()), Pearson = mean(Pearson, na.rm = T))  %>%
+    select(Environment, Trait, Pearson, SE_Pearson, MSEP, SE_MSEP) %>%
+    mutate_if(is.numeric, funs(round(., digits))) %>%
+    as.data.frame() -> finalSum
+
+  out <- switch(information,
+                compact = finalSum,
+                complete = presum,
+                extended = {
+                  finalSum$Partition <- 'All'
+                  presum$Partition <- as.character(presum$Partition)
+                  presum$SE_Pearson <- NA
+                  presum$SE_MSEP <- NA
+                  rbind(presum, finalSum)
+                }
+  )
+  return(out)
+}
 
 
 #' Print BMTME information object
@@ -101,7 +141,7 @@ summary.BMECV <- function(object, information = 'compact', digits = 4, ...){
 #' @export
 #'
 print.BMTME <- function(x, ...){
-  cat('Fitted Multi-Trait Multi-Environment Model with: \n',
+  cat('Fitted Bayesian Multi-Trait Multi-Environment Model with: \n',
       x$nIter, ' Iterations, burning the first ', x$burnIn, ' and thining every ', x$thin, '\n',
       'We found ', x$NAvalues, ' NA values \n\n',
       'Predicted Values: \n')
@@ -128,6 +168,79 @@ print.BME <- function(x, ...){
       'Predicted Values: \n')
 
   print.default(format(x$yHat, digits = 3), print.gap = 2L, quote = FALSE)
+
+  cat('\n Use str() function to found more datailed information.')
+  invisible(x)
+}
+
+#' Print BMTMECV information object
+#'
+#' @param x object a
+#' @param ...  more objects
+#'
+#' @return test
+#' @export
+#'
+print.BMTMECV <- function(x, ...){
+  cat('Fitted Bayesian Multi-Trait Multi-Environment Model with: \n',
+      x$nIter, ' Iterations, burning the first ', x$burnIn, ' and thining every ', x$thin, '\n',
+      'Runtime: ', x$executionTime ,' seconds \n\n',
+      'Some predicted values: \n')
+
+  print.default(format(head(x$results$Predicted, 20), digits = 3), print.gap = 2L, quote = FALSE)
+
+  cat('\nPredictive capacity of the model: \n')
+
+  print.data.frame(summary(x, 'compact', digits = 3), print.gap = 2L, quote = FALSE)
+
+  cat('\n Use str() function to found more datailed information.')
+  invisible(x)
+}
+
+#' Print BMECV information object
+#'
+#' @param x object a
+#' @param ...  more objects
+#'
+#' @return test
+#' @export
+#'
+print.BMECV <- function(x, ...){
+  cat('Fitted Bayesian Multi Environment model with: \n',
+      x$nIter, ' Iterations, burning the first ', x$burnIn, ' and thining every ', x$thin, '\n',
+      'Runtime: ', x$executionTime ,' seconds \n\n',
+      'Some predicted values: \n')
+
+  print.default(format(head(x$results$Predicted, 20), digits = 3), print.gap = 2L, quote = FALSE)
+
+  cat('\nPredictive capacity of the model: \n')
+
+  print.data.frame(summary(x, 'compact', digits = 3), print.gap = 2L, quote = FALSE)
+
+  cat('\n Use str() function to found more datailed information.')
+  invisible(x)
+}
+
+
+#' Print BMTMERSCV information object
+#'
+#' @param x object a
+#' @param ...  more objects
+#'
+#' @return test
+#' @export
+#'
+print.BMTMERSCV <- function(x, ...){
+  cat('Fitted Bayesian Multi Trait Multi Environment Regressor Stacking model with: \n',
+      x$nIter, ' Iterations, burning the first ', x$burnIn, ' and thining every ', x$thin, '\n',
+      'Runtime: ', x$executionTime ,' seconds \n\n',
+      'Some predicted values: \n')
+
+  print.default(format(head(x$results$Predicted, 20), digits = 3), print.gap = 2L, quote = FALSE)
+
+  cat('\nPredictive capacity of the model: \n')
+
+  print.data.frame(summary(x, 'compact', digits = 3), print.gap = 2L, quote = FALSE)
 
   cat('\n Use str() function to found more datailed information.')
   invisible(x)
@@ -196,6 +309,40 @@ plot.BMTME <- function(x, ...){
   plot(response, predictions, main = "BMTME fitted model", xlim = limits, ylim = limits, xlab = 'Response', ylab = 'Prediction', ...);
   abline(a = 0, b = 1, lty = 3)
 }
+#' @title Plot BMTMERSCV graph
+#'
+#' @description Plot from BMTMERSCV object
+#'
+#' @param x \code{BMTMERSCV object} BMTMERSCV object, result of use the MTME() function
+#' @param select \code{character} By default ('Pearson'), plot the Pearson Correlations of the MTME Object, else ('MSEP'), plot the MSEP of the BMTMERSCV Object.
+#' @param ... Further arguments passed to or from other methods.
+#'
+#' @importFrom graphics arrows axis plot
+#' @export
+plot.BMTMERSCV <- function(x, select = 'Pearson', ...){
+  ### Check that object is compatible
+  if (!inherits(x, "BMTMERSCV")) Error("This function only works for objects of class 'BMTMERSCV'")
+
+  results <- summary(x)
+  results <- results[order(results[, select]),]
+
+  if (select == "Pearson") {
+    results$SE <- 1.96 * results$SE_Pearson
+    ylab <- "Pearson's Correlation"
+  } else if (select == "MSEP") {
+
+    results$SE <- 1.96 * results$SE_MSEP[which(results$Fold == 'Average_all')]
+    ylab <- select
+  }
+  x.labels <- paste0(results$Trait, '_', results$Env)
+  plot.x <- 1:length(x.labels)
+  plot(plot.x, results[, select], ylim = range(c(results[, select] - results$SE, results[, select] + results$SE)),
+       type = 'p', ylab = ylab, xlab = '', xaxt = "n", ...)
+  axis(1, at = plot.x, labels = x.labels, las = 2)
+  arrows(plot.x, results[, select] - results$SE, plot.x, results[, select] + results$SE, code = 3, length = 0.02, angle = 90)
+}
+
+
 
 #' @title boxplot.BMECV
 #'
@@ -276,3 +423,50 @@ boxplot.BMTMECV <- function(x, select = 'Pearson', ordered = TRUE, ...){
     boxplot(plot.y, col = "grey", xlab = 'Environment', ylab = ylab, ...)
   }
 }
+
+#' @title boxplot.BMTMERSCV
+#'
+#' @description Solo es una prueba
+#'
+#' @param x \code{BMTMERSCV object} Objeto BMTMERSCV, resultado de ejecutar MTME()
+#' @param select \code{string} Pearson or MSEP
+#' @param ordered \code{logic} TRUE or FALSE
+#' @param ... Further arguments passed to or from other methods.
+#'
+#' @importFrom graphics boxplot
+#' @export
+boxplot.BMTMERSCV <- function(x, select = 'Pearson', ordered = TRUE, ...){
+  ### Check that object is compatible
+  if (!inherits(x, "BMTMERSCV")) Error("This function only works for objects of class 'BMTMERSCV'")
+
+  results <- summary(x, 'complete')
+
+  switch (select,
+          Pearson = {
+            plot.y <- results$Pearson
+            ylab <- "Pearson's Correlation"
+          }, MSEP = {
+            plot.y <- results$MSEP
+            ylab <- "MSEP Average"
+          }, CC = {
+            plot.y <- results$CC
+            ylab <- "Classification correct average"
+          },
+          Error('Error in select parameter.')
+  )
+
+  if (length(unique(results$Env)) > 1) {
+    results$TxE <- paste0(results$Trait, '_', results$Env)
+
+    if (ordered && select != 'MSEP') {
+      results$TxE  <- with(results, reorder(TxE , Pearson, median, na.rm = T))
+    } else if (ordered && select == 'MSEP') {
+      results$TxE  <- with(results, reorder(TxE , MSEP, median, na.rm = T))
+    }
+
+    boxplot(plot.y ~ results$TxE, col = "grey", ylab = ylab, ...)
+  }else{
+    boxplot(plot.y, col = "grey", ylab = ylab, ...)
+  }
+}
+
