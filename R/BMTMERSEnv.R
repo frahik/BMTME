655f3d3370120c5cb1b1956567a6d832382dfedf
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-08-30 22:23:03 -0500

    fixed BMTMERSEnv function
    * add covModel parameter
    * Easy-to-read bar progress
    * removed covariables export
    * fixed estimation model

--- R/BMTMERSEnv.R
+++ R/BMTMERSEnv.R
@@ -17,57 +17,49 @@
 #' @examples
 #' ETA=list(Env=list(X=Z.E,model="BRR"),Gen = list(X = Z.G, model = 'BRR'), EnvGen=list(X=Z.EG,model="BRR"))
 #' testingSet <- BMTME::CV.RandomPart(pheno, NPartitions = 10, PTesting = 0.2, set_seed = 123)
-BMTMERS_Env <- function(data = NULL, testingEnv = '', ETA = NULL, nIter = 2500, burnIn = 500, thin = 5, progressBar = TRUE, digits = 4) {
+BMTMERS_Env <- function(data = NULL, testingEnv = '', ETA = NULL, covModel = 'BRR', nIter = 2500, burnIn = 500, thin = 5, progressBar = TRUE, digits = 4) {
   time.init <- proc.time()[3]
   Y <- data[, -1]
-  YwithCov <-  Y # to include covariance data
-  nTraits <- dim(Y)[2L]
+  YwithCov <-  Y # to include covariable data
+  nTraits <- dim(Y)[2]
   nEnvs <- length(testingEnv)
   results <- data.frame() # save cross-validation results
   pb <- progress::progress_bar$new(format = ':what  [:bar]:percent;  Time elapsed: :elapsed - time left: :eta', total = 2L*(nEnvs*nTraits), clear = FALSE, show_after = 0)
-
   #########First stage analysis#################################
-  for (trait in seq_len(nTraits)) {
-    y2 <- Y[, trait]
-    predictions <- list()
-    for (env in testingEnv) {
+  for (env in testingEnv) {
+    Pos_ts <- which(data[, 1] == env)
+    for (trait in seq_len(nTraits)) {
       if (progressBar) {
-        pb$tick(tokens = list(what = paste0('Estimating covariance of trait ', colnames(Y)[trait], ' in the environment ', env)))
+        pb$tick(tokens = list(what = paste0('Estimating covariates')))
       }
-      y1 <- y2
-      Pos_ts <- which(data[, 1] == env)
+      y1 <- Y[, trait]
       y1[Pos_ts] <- NA
 
       fm <- BGLR(y = y1, ETA = ETA, nIter = nIter, burnIn = burnIn, verbose = F)
-      predictions[[env]] <- fm$yHat
+      YwithCov[, nTraits + trait] <- fm$yHat
     }
-    envPred <- matrix(unlist(predictions), ncol = length(testingEnv))
-    YwithCov[, nTraits + trait] <- apply(envPred, 1, mean)
-  }
-  XPV <- scale(YwithCov[, (1L + nTraits):(2L*nTraits)])
 
-  ETA1 <- ETA
-  ETA1$Cov_PreVal <- list(X = XPV, model = "BRR")
+    XPV <- scale(YwithCov[, (1L + nTraits):(2L*nTraits)])
+    ETA1 <- ETA
+    ETA1$Cov_PreVal <- list(X = XPV, model = covModel)
 
-  for (t in seq_len(nTraits)) {
-    y2 <- Y[, t]
-    for (env in testingEnv) {
+    for (t in seq_len(nTraits)) {
+      y2 <- Y[, t]
       if (progressBar) {
-        pb$tick(tokens = list(what = paste0('Fitting the model in the trait ', colnames(Y)[t], ' and the environment ', env, ' for the global estimations')))
+        pb$tick(tokens = list(what = paste0('Fitting the model')))
       }
       y1 <- y2
-      Pos_ts <- which(data[, 1] == env)
       y1[Pos_ts] <- NA
       fm <- BGLR(y1, ETA = ETA1, nIter = nIter, burnIn = burnIn, thin = thin, verbose = F)
-
       results <- rbind(results, data.frame(Position = Pos_ts,
                                            Environment = data[Pos_ts, 1],
                                            Trait = colnames(Y)[t],
                                            Observed = round(y2[Pos_ts], digits),
                                            Predicted = round(fm$yHat[Pos_ts], digits)))
+
     }
   }
-  out <- list(results = results, covariance = XPV, nIter = nIter, burnIn = burnIn, thin = thin, executionTime = proc.time()[3] - time.init)
+  out <- list(results = results, nIter = nIter, burnIn = burnIn, thin = thin, executionTime = proc.time()[3] - time.init)
   class(out) <- 'BMTMERSENV'
   return(out)
 }
