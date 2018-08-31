615d9c1e6c8510de1a528b644aa863528f01d8b8
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-08-31 13:16:41 -0500

    include validate parallelCores function

--- R/detectCore.R
+++ R/detectCore.R
@@ -7,3 +7,13 @@
 #' @importFrom parallel detectCores
 #' @usage detectCores()
 NULL
+
+validate.parallelCores <-  function(parallelCores){
+  nCore <- detectCores()
+  if (nCore < parallelCores) {
+    message(paste0('[!] There not more than ',  nCore, 'availabe, for safe analysis, we stablish ', nCore - 1, 'to do the analysis'))
+    return(nCore - 1)
+  } else {
+    return(parallelCores)
+  }
+}
