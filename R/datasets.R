1d172edacef0847dccc7ad2fb7f0f633f1fd069d
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-09-02 15:55:17 -0500

    include datasets r files

---
+++ R/datasets.R
@@ -0,0 +1,70 @@
+#' @name WheatToy
+#' @title Phenotypic values and Genetic values of Wheat toy dataset.
+#' @description A data set based on a portion of the data used in the study of...
+#' The variables are as follows:
+#'
+#' * 'phenoWheatToy': Phenotypic values.
+#' * 'genoWheatToy':  Geno values.
+#'
+#' @docType data
+#' @usage data(WheatToy)
+#' @format format, 300 row per 4 columns.
+#' @author Luna-Vázquez, Francisco Javier
+#'
+NULL
+
+#' @name WheatIranianToy
+#' @title Wheat Iranian Toy Data
+#' @description The package includes a data set based on a portion of the data used in the study of
+#'
+#' @docType data
+#' @usage data(WheatIranianToy)
+#' @format format, 300 row per 4 columns.
+#' @author Luna-Vázquez, Francisco Javier
+#'
+NULL
+
+#' @name WheatJapa50
+#' @title WheatJapa50 Data
+#' @description The package includes a data set based on a portion of the data used in the study of
+#'
+#' @docType data
+#' @usage data(WheatJapa50)
+#' @format format, 300 row per 4 columns.
+#' @author Luna-Vázquez, Francisco Javier
+#'
+NULL
+
+
+#' @name WheatJapa30
+#' @title WheatJapa30 Data
+#' @description The package includes a data set based on a portion of the data used in the study of
+#'
+#' @docType data
+#' @usage data(WheatJapa30)
+#' @format format, 300 row per 4 columns.
+#' @author Luna-Vázquez, Francisco Javier
+#'
+NULL
+
+#' @name WheatMadaToy
+#' @title WheatMadaToy Data
+#' @description The package includes a data set based on a portion of the data used in the study of
+#'
+#' @docType data
+#' @usage data(WheatMadaToy)
+#' @format format, 300 row per 4 columns.
+#' @author Luna-Vázquez, Francisco Javier
+#'
+NULL
+
+#' @name MaizeToy
+#' @title MaizeToy Data
+#' @description The package includes a data set based on a portion of the data used in the study of
+#'
+#' @docType data
+#' @usage data(MaizeToy)
+#' @format format, 300 row per 4 columns.
+#' @author Luna-Vázquez, Francisco Javier
+#'
+NULL
