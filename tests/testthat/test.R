de1adb014528450ecd648b8be23b1b536f29d409
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-10-28 14:32:17 -0600

    Include more test

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,43 +1,131 @@
 library(testthat)
 library(BMTME)
 
-context('Fit and predict BME model with Mada data')
-
-test_that('Fit and predict Mada data', {
+context('Partitions test')
+test_that('K-Folds', {
   data("WheatMadaToy")
+  phenoMada <- (phenoMada[order(phenoMada$GID),])
+  pheno <- data.frame(GID = phenoMada[, 1], Env = 'Unique', Response = phenoMada[, 3])
+
+  KFold_Test <- CV.KFold(pheno, set_seed = 123)
+
+  expect_output(str(KFold_Test), 'List of 5')
+  expect_is(KFold_Test, 'CrossValidation')
+  expect_is(KFold_Test$CrossValidation_list, 'list')
+  expect_output(str(KFold_Test$CrossValidation_list), 'List of 5')
+  expect_is(KFold_Test$ng, 'numeric')
+  expect_is(KFold_Test$Environments, 'factor')
+  expect_length(KFold_Test$Environments, length(phenoMada$GID))
+  expect_equal(unique(KFold_Test$Environments), unique(pheno$Env))
+  expect_is(KFold_Test$Traits, 'character')
+  expect_length(KFold_Test$Traits, length(phenoMada$GID))
+})
+
+test_that('RandomPartition',{
+  data("MaizeToy")
+  phenoMaizeToy <- phenoMaizeToy[order(phenoMaizeToy$Env, phenoMaizeToy$Line), ]
+  pheno <- data.frame(GID = phenoMaizeToy[, 1], Env = phenoMaizeToy$Env, Response = phenoMaizeToy[, 3])
+
+  RP_Test <- CV.RandomPart(pheno, set_seed = 123)
+
+  expect_output(str(RP_Test), 'List of 6')
+  expect_is(RP_Test, 'CrossValidation')
+  expect_is(RP_Test$CrossValidation_list, 'list')
+  expect_output(str(RP_Test$CrossValidation_list), 'List of 10')
+  expect_is(RP_Test$Environments, 'factor')
+  expect_length(RP_Test$Environments, length(pheno$Env))
+  expect_equal(unique(RP_Test$Environments), unique(pheno$Env))
+  expect_is(RP_Test$Traits, 'character')
+  expect_length(RP_Test$Traits, length(pheno$Env))
+})
+
+test_that('Stratified by number',{
+  data("MaizeToy")
+  phenoMaizeToy <- phenoMaizeToy[order(phenoMaizeToy$Env, phenoMaizeToy$Line), ]
+  pheno <- data.frame(GID = phenoMaizeToy[, 1], Env = phenoMaizeToy$Env, Response = phenoMaizeToy[, 3])
+
+  SN_Test <- CV.Stratified(pheno, set_seed = 123)
+
+  expect_output(str(SN_Test), 'List of 3')
+  expect_is(SN_Test, 'CrossValidation')
+  expect_is(SN_Test$CrossValidation_list, 'list')
+  expect_output(str(SN_Test$CrossValidation_list), 'List of 10')
+  expect_is(SN_Test$Environments, 'factor')
+  expect_length(SN_Test$Environments, length(pheno$Env))
+  expect_equal(unique(SN_Test$Environments), unique(pheno$Env))
+  expect_is(SN_Test$Traits, 'character')
+  expect_length(SN_Test$Traits, length(pheno$Env))
+})
+
+test_that('Stratified by fraction',{
+  data("MaizeToy")
+  phenoMaizeToy <- phenoMaizeToy[order(phenoMaizeToy$Env, phenoMaizeToy$Line), ]
+  pheno <- data.frame(GID = phenoMaizeToy[, 1], Env = phenoMaizeToy$Env, Response = phenoMaizeToy[, 3])
+
+  SF_Test <- CV.Stratified(pheno, set_seed = 123)
+
+  expect_output(str(SF_Test), 'List of 3')
+  expect_is(SF_Test, 'CrossValidation')
+  expect_is(SF_Test$CrossValidation_list, 'list')
+  expect_output(str(SF_Test$CrossValidation_list), 'List of 10')
+  expect_is(SF_Test$Environments, 'factor')
+  expect_length(SF_Test$Environments, length(pheno$Env))
+  expect_equal(unique(SF_Test$Environments), unique(pheno$Env))
+  expect_is(SF_Test$Traits, 'character')
+  expect_length(SF_Test$Traits, length(pheno$Env))
+})
+
+context('Predictions')
+
+test_that('BME function with Mada data', {
+  data("WheatMadaToy")
+  phenoMada <- (phenoMada[order(phenoMada$GID),])
 
   LG <- cholesky(genoMada)
   ZG <- model.matrix(~0 + as.factor(phenoMada$GID))
   Z.G <- ZG %*% LG
-
   Y <- as.matrix(phenoMada[, -c(1)])
 
+  # Check fitting
   fm <- BME(Y = Y, Z1 = Z.G, nIter = 100, burnIn = 50, thin = 2, bs = 50)
-
-  pheno <- data.frame(GID = phenoMada[, 1], Env = '', Response = phenoMada[, 3])
-  CrossV <- CV.RandomPart(pheno, NPartitions = 4, PTesting = 0.2, set_seed = 123)
-
-  pm <- BME(Y = Y, Z1 = Z.G, nIter = 10, burnIn = 5, thin = 2, bs = 50, testingSet = CrossV)
-  pm_parallel <- BME(Y = Y, Z1 = Z.G, nIter = 10, burnIn = 5, thin = 2, bs = 50, testingSet = CrossV, parallelCores = 2)
 
   expect_is(fm, 'BME')
   expect_output(str(fm), 'List of 17')
   expect_is(fm$Y, 'matrix')
+  expect_is(fm$yHat, 'matrix')
+  expect_equal(dim(fm$Y), dim(fm$yHat))
+  expect_equal(fm$NAvalues, 0)
+  expect_output(print(fm), 'Multi-Environment Model Fitted with')
+  expect_silent(plot(fm, trait = 'PH'))
+
+  # Check predictive capacities of the model
+  pheno <- data.frame(GID = phenoMada[, 1], Env = '', Response = phenoMada[, 3])
+  CrossV <- CV.RandomPart(pheno, NPartitions = 4, PTesting = 0.2, set_seed = 123)
+
+  pm <- BME(Y = Y, Z1 = Z.G, nIter = 10, burnIn = 5, thin = 2, bs = 50, testingSet = CrossV)
 
   expect_output(str(pm), 'List of 6')
   expect_is(pm, 'BMECV')
   expect_is(pm$results, 'data.frame')
   expect_is(pm$executionTime, 'numeric')
+  expect_output(print(pm), 'Fitted Bayesian Multi Environment model with:')
+  expect_silent(boxplot(pm))
+
+  # Check the work in parallel
+  pm_parallel <- BME(Y = Y, Z1 = Z.G, nIter = 10, burnIn = 5, thin = 2, bs = 50, testingSet = CrossV, parallelCores = 2)
 
   expect_output(str(pm_parallel), 'List of 6')
   expect_is(pm_parallel, 'BMECV')
   expect_is(pm_parallel$results, 'data.frame')
   expect_is(pm_parallel$executionTime, 'numeric')
+  expect_output(print(pm_parallel), 'Fitted Bayesian Multi Environment model with:')
+  expect_silent(boxplot(pm_parallel))
+
+  expect_error(expect_equivalent(pm, pm_parallel))
 })
 
-context('Fit and predict BMTME model with Iranian data')
 
-test_that('Fit and predict Iranian data', {
+test_that('BMTME function with Iranian data', {
   data("WheatIranianToy")
 
   LG <- cholesky(genoIranianToy)
@@ -50,28 +138,99 @@ test_that('Fit and predict Iranian data', {
   Z.EG <- ZEG %*% LG2
   Y <- as.matrix(phenoIranianToy[, -c(1, 2)])
 
+  #Check fitting
   fm <- BMTME(Y = Y, X = Z.E, Z1 = Z.G, Z2 = Z.EG, nIter = 100, burnIn = 50, thin = 2, bs = 50)
-
-  pheno <- data.frame(GID = phenoIranianToy[, 1], Env = phenoIranianToy[, 2], Response = phenoIranianToy[, 3])
-
-  CrossV <- CV.RandomPart(pheno, NPartitions = 4, PTesting = 0.2, set_seed = 123)
-
-  pm <- BMTME(Y = Y, X = Z.E, Z1 = Z.G, Z2 = Z.EG, nIter = 10, burnIn = 5, thin = 2, bs = 50, testingSet = CrossV)
-  pm_parallel <- BMTME(Y = Y, X = Z.E, Z1 = Z.G, Z2 = Z.EG, nIter = 10, burnIn = 5, thin = 2, bs = 50, testingSet = CrossV, parallelCores = 2)
-
   expect_is(fm, 'BMTME')
   expect_output(str(fm), 'List of 19')
   expect_is(fm$Y, 'matrix')
+  expect_is(fm$yHat, 'matrix')
+  expect_equal(dim(fm$Y), dim(fm$yHat))
+  expect_equal(fm$NAvalues, 0)
+  expect_output(print(fm), 'Fitted Bayesian Multi-Trait Multi-Environment Model with:')
+  expect_silent(plot(fm, trait = 'DTH'))
+
+  # Check predictive capacities of the model
+  pheno <- data.frame(GID = phenoIranianToy[, 1], Env = phenoIranianToy[, 2], Response = phenoIranianToy[, 3])
+
+  CrossV <- CV.RandomPart(pheno, NPartitions = 4, PTesting = 0.2, set_seed = 123)
+  pm <- BMTME(Y = Y, X = Z.E, Z1 = Z.G, Z2 = Z.EG, nIter = 10, burnIn = 5, thin = 2, bs = 50, testingSet = CrossV)
 
   expect_output(str(pm), 'List of 6')
   expect_is(pm, 'BMTMECV')
   expect_is(pm$results, 'data.frame')
   expect_is(pm$executionTime, 'numeric')
+  expect_output(print(pm), 'Fitted Bayesian Multi-Trait Multi-Environment Model with:')
+  expect_silent(boxplot(pm))
+
+  # Check the work in parallel
+  pm_parallel <- BMTME(Y = Y, X = Z.E, Z1 = Z.G, Z2 = Z.EG, nIter = 10, burnIn = 5, thin = 2, bs = 50, testingSet = CrossV, parallelCores = 2)
 
   expect_output(str(pm_parallel), 'List of 6')
   expect_is(pm_parallel, 'BMTMECV')
   expect_is(pm_parallel$results, 'data.frame')
   expect_is(pm_parallel$executionTime, 'numeric')
+  expect_output(print(pm_parallel), 'Fitted Bayesian Multi-Trait Multi-Environment Model with:')
+  expect_silent(boxplot(pm_parallel))
+
+  expect_error(expect_equivalent(pm, pm_parallel))
 })
 
+test_that('BMORS function with Wheat data', {
+  data("WheatToy")
+  phenoWheatToy <- phenoWheatToy[order(phenoWheatToy$Env, phenoWheatToy$Gid),]
+  LG <- cholesky(genoWheatToy)
+  ZG <- model.matrix(~0 + as.factor(phenoWheatToy$Gid))
+  Z.G <- ZG %*% LG
+  ETA <- list(Gen = list(X = Z.G, model = 'BRR'))
 
+  pheno <- phenoWheatToy[, c(1:3)] #Use only the first trait to do a cv
+  colnames(pheno) <- c('Line', 'Env', 'Response')
+  CrossValidation <- CV.RandomPart(pheno, NPartitions = 10, PTesting = 0.2, set_seed = 123)
+  Y <- as.matrix(phenoWheatToy[, c(3,4)])
+  # Check predictive capacities of the model
+  pm <- BMORS(Y, ETA = ETA, nIter = 10, burnIn = 5, thin = 2, progressBar = FALSE,
+              testingSet = CrossValidation,  digits = 4)
+
+  expect_output(str(pm), 'List of 5')
+  expect_is(pm, 'BMORSCV')
+  expect_is(pm$results, 'data.frame')
+  expect_is(pm$executionTime, 'numeric')
+  expect_output(print(pm), 'Fitted Bayesian Multi-Output Regression Stacking model with:')
+  expect_silent(boxplot(pm))
+
+  # Check the work in parallel
+  pm_parallel <- BMORS(Y, ETA = ETA, nIter = 10, burnIn = 5, thin = 2, progressBar = FALSE,
+                       testingSet = CrossValidation,  digits = 4, parallelCores = 2)
+
+  expect_output(str(pm_parallel), 'List of 5')
+  expect_is(pm_parallel, 'BMORSCV')
+  expect_is(pm_parallel$results, 'data.frame')
+  expect_is(pm_parallel$executionTime, 'numeric')
+  expect_output(print(pm_parallel), 'Fitted Bayesian Multi-Output Regression Stacking model with:')
+  expect_silent(boxplot(pm_parallel))
+
+  expect_error(expect_equivalent(pm, pm_parallel))
+})
+
+test_that('BMORS_Env function with Maize data', {
+  data('MaizeToy')
+  phenoMaizeToy <- phenoMaizeToy[order(phenoMaizeToy$Env, phenoMaizeToy$Line),]
+  LG <- cholesky(genoMaizeToy)
+  ZG <- model.matrix(~0 + as.factor(phenoMaizeToy$Line))
+  Z.G <- ZG %*% LG
+  ETA <- list(Gen = list(X = Z.G, model = 'BRR'))
+
+  dataset <- phenoMaizeToy[, 2:5] #Must Include in the first column the environments
+
+  # Check predictive capacities of the model
+  pm <- BMORS_Env(dataset, testingEnv = 'EBU', ETA = ETA, covModel = 'BRR', nIter = 10,
+                  burnIn = 4, thin = 2, progressBar = FALSE, digits = 3)
+
+  expect_output(str(pm), 'List of 5')
+  expect_is(pm, 'BMORSENV')
+  expect_is(pm$results, 'data.frame')
+  expect_is(pm$executionTime, 'numeric')
+  expect_output(print(pm), 'Fitted Bayesian Multi-Output Regression Stacking model for n environments with:')
+  expect_silent(barplot(pm))
+
+})
