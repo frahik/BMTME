c6172bf3421aef6ce6e0de200cfef8c8942b782b
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-11-24 00:18:11 -0600

    update tests

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -39,7 +39,29 @@ test_that('RandomPartition',{
   expect_length(RP_Test$Traits, length(pheno$Env))
 })
 
-context('Predictions')
+context('BME function')
+test_that('BME function with Mada data with NA values', {
+  data("WheatMadaToy")
+  phenoMada <- (phenoMada[order(phenoMada$GID),])
+  phenoMada[sample.int(50, 15), 2:7] <- NA
+
+  LG <- cholesky(genoMada)
+  ZG <- model.matrix(~0 + as.factor(phenoMada$GID))
+  Z.G <- ZG %*% LG
+  Y <- as.matrix(phenoMada[, -c(1)])
+
+  # Check fitting
+  fm <- BME(Y = Y, Z1 = Z.G, nIter = 100, burnIn = 50, thin = 2, bs = 50)
+
+  expect_is(fm, 'BME')
+  expect_output(str(fm), 'List of 17')
+  expect_is(fm$Y, 'matrix')
+  expect_is(fm$yHat, 'matrix')
+  expect_equal(dim(fm$Y), dim(fm$yHat))
+  expect_gt(fm$NAvalues, 0)
+  expect_output(print(fm), 'Multi-Environment Model Fitted with')
+  expect_silent(plot(fm, trait = 'PH'))
+})
 
 test_that('BME function with Mada data', {
   data("WheatMadaToy")
@@ -87,6 +109,7 @@ test_that('BME function with Mada data', {
   # Check the work in parallel
   pm_parallel <- BME(Y = Y, Z1 = Z.G, nIter = 10, burnIn = 5, thin = 2, bs = 50, testingSet = CrossV, parallelCores = 2)
 
+  expect_message(BME(Y = Y, Z1 = Z.G, nIter = 10, burnIn = 5, thin = 2, bs = 50, testingSet = CrossV, parallelCores = detectCores() + 1), regexp = '[!]')
   expect_output(str(pm_parallel), 'List of 6')
   expect_is(pm_parallel, 'BMECV')
   expect_is(pm_parallel$results, 'data.frame')
@@ -97,6 +120,51 @@ test_that('BME function with Mada data', {
   expect_error(expect_equivalent(pm, pm_parallel))
 })
 
+context('BMTME function')
+
+test_that('BMTME function with Iranian data with NA values', {
+  data("WheatIranianToy")
+  removePos <- sample.int(50, 15)
+  phenoIranianToy <- (phenoIranianToy[order(phenoIranianToy$GID),])
+  phenoIranianToy$DTH[removePos] <- NA
+
+  LG <- cholesky(genoIranianToy)
+  ZG <- model.matrix(~0 + as.factor(phenoIranianToy$GID))
+  Z.G <- ZG %*% LG
+  Z.E <- model.matrix(~0 + as.factor(phenoIranianToy$Env))
+  ZEG <- model.matrix(~0 + as.factor(phenoIranianToy$GID):as.factor(phenoIranianToy$Env))
+  G2 <- kronecker(diag(length(unique(phenoIranianToy$Env))), data.matrix(genoIranianToy))
+  LG2 <- cholesky(G2)
+  Z.EG <- ZEG %*% LG2
+  Y <- as.matrix(phenoIranianToy[, -c(1, 2)])
+
+  #Check fitting
+  fm <- BMTME(Y = Y, X = Z.E, Z1 = Z.G, Z2 = Z.EG, nIter = 100, burnIn = 50, thin = 2, bs = 50)
+  expect_is(fm, 'BMTME')
+  expect_output(str(fm), 'List of 19')
+  expect_is(fm$Y, 'matrix')
+  expect_is(fm$yHat, 'matrix')
+  expect_equal(dim(fm$Y), dim(fm$yHat))
+  expect_gt(fm$NAvalues, 0)
+  expect_output(print(fm), 'Fitted Bayesian Multi-Trait Multi-Environment Model with:')
+  expect_silent(plot(fm, trait = 'DTH'))
+
+  # With more traits removed
+  phenoIranianToy[removePos, 3:4] <- NA
+  Y <- as.matrix(phenoIranianToy[, -c(1, 2)])
+
+  #Check fitting
+  fm <- BMTME(Y = Y, X = Z.E, Z1 = Z.G, Z2 = Z.EG, nIter = 100, burnIn = 50, thin = 2, bs = 50)
+  expect_is(fm, 'BMTME')
+  expect_output(str(fm), 'List of 19')
+  expect_is(fm$Y, 'matrix')
+  expect_is(fm$yHat, 'matrix')
+  expect_equal(dim(fm$Y), dim(fm$yHat))
+  expect_gt(fm$NAvalues, 0)
+  expect_output(print(fm), 'Fitted Bayesian Multi-Trait Multi-Environment Model with:')
+  expect_silent(plot(fm, trait = 'DTH'))
+
+})
 
 test_that('BMTME function with Iranian data', {
   data("WheatIranianToy")
@@ -128,7 +196,7 @@ test_that('BMTME function with Iranian data', {
   CrossV <- CV.RandomPart(pheno, NPartitions = 4, PTesting = 0.2, set_seed = 123)
   pm_basic <- BMTME(Y = Y, X = Z.E, Z1 = Z.G, Z2 = Z.EG, nIter = 10, burnIn = 5, thin = 2, bs = 50, testingSet = CrossV$CrossValidation_list$partition1)
 
-  expect_output(str(pm_basic), 'List of 6')
+  expect_output(str(pm_basic), 'List of 5')
   expect_is(pm_basic, 'BMTMECV')
   expect_is(pm_basic$results, 'data.frame')
   expect_is(pm_basic$executionTime, 'numeric')
@@ -158,7 +226,32 @@ test_that('BMTME function with Iranian data', {
   expect_error(expect_equivalent(pm, pm_parallel))
 })
 
-test_that('BMORS function with Wheat data', {
+context('BMORS function')
+test_that('BMORS function with Wheat data with NA values', {
+  data("WheatToy")
+  phenoWheatToy <- phenoWheatToy[order(phenoWheatToy$Env, phenoWheatToy$Gid),]
+  phenoWheatToy[sample.int(50, 15), 3] <- NA
+  phenoWheatToy[sample.int(50, 15), 4] <- NA
+
+  LG <- cholesky(genoWheatToy)
+  ZG <- model.matrix(~0 + as.factor(phenoWheatToy$Gid))
+  Z.G <- ZG %*% LG
+  ETA <- list(Gen = list(X = Z.G, model = 'BRR'))
+
+  Y <- as.matrix(phenoWheatToy[, c(3,4)])
+  # Check predictive capacities of the model
+  pm <- BMORS(Y, ETA = ETA, nIter = 10, burnIn = 5, thin = 2, progressBar = FALSE,
+              digits = 4)
+  expect_output(str(pm), 'List of 6')
+  expect_is(pm, 'BMORS')
+  expect_is(pm$results, 'data.frame')
+  expect_true(all(is.na(pm$results$Observed)))
+  expect_gt(pm$NAvalues, 0)
+  expect_is(pm$executionTime, 'numeric')
+  expect_output(print(pm), 'Fitted Bayesian Multi-Output Regression Stacking model with:')
+})
+
+test_that('BMORS function with Wheat full data', {
   data("WheatToy")
   phenoWheatToy <- phenoWheatToy[order(phenoWheatToy$Env, phenoWheatToy$Gid),]
   LG <- cholesky(genoWheatToy)
@@ -195,6 +288,7 @@ test_that('BMORS function with Wheat data', {
   expect_error(expect_equivalent(pm, pm_parallel))
 })
 
+context('BMORS_Env function')
 test_that('BMORS_Env function with Maize data', {
   data('MaizeToy')
   phenoMaizeToy <- phenoMaizeToy[order(phenoMaizeToy$Env, phenoMaizeToy$Line),]
