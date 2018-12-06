af379509aa228bbdd88a32ad709de82d1c6a2e9c
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-12-06 17:28:24 -0600

    Refactoring code

--- R/cholesky.R
+++ R/cholesky.R
@@ -2,43 +2,29 @@
 #'
 #' Compute the Cholesky factorization of a non-real symmetric positive-definite square matrix.
 #'
-#' @param a (numeric - matrix) an object to apply this method, it could be non positive-definite matrices.
+#' @param G (numeric - matrix) an object to apply this method, it could be non positive-definite matrices.
 #'
 #' @export
 #'
-cholesky <- function(a) {
-  n <- dim(a)[1]
-  root <- matrix(0, n, n)
-
-  for (i in 1:n) {
-    sum2 <- 0
-    if (i > 1) {
-      sum2 <- sum(root[i, 1:(i - 1)] ^ 2)
-    }
-
-    x <- a[i, i] - sum2
-
-    if (x < 0) {
-      x <- 0
-    }
-
-    root[i, i] <- sqrt(x)
-
-    if (i < n) {
-      for (j in (i + 1):n) {
-        if (root[i, i] == 0) {
-          x <- 0
-        }
-        else {
-          sum2 <- 0
-          if (i > 1) {
-            sum2 <- root[i, 1:(i - 1)] %*% t(t(root[j, 1:(i - 1)]))
-          }
-          x <- (a[i, j] - sum2) / root[i, i]
-        }
-        root[j, i] <- x
-      }
-    }
+cholesky <- function(G) {
+  EigenA <- eigen(G)
+  d_A    <- EigenA$values
+  V_A    <- EigenA$vectors
+  pos_A1 <- which(d_A > 1e-10)
+  if (identical(pos_A1, integer(0))) {
+    pos_A <- 1L
+  } else {
+    pos_A <- pos_A1
   }
-  return(root)
+  d_A_Star <- d_A[pos_A]
+  V_A_Star <- V_A[, pos_A]
+
+  if (length(pos_A) == 1L) {
+    d_A_Star <- 1 / d_A_Star
+    LG <- d_A_Star * sqrt(V_A_Star)
+  } else {
+    d_A_Star <- diag(d_A_Star)
+    LG <- (V_A_Star %*% sqrt(d_A_Star))
+  }
+  return(LG)
 }
