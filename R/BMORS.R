5451f72296a9dd9bad5ccc62fb3538a026e64533
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-12-07 15:47:09 -0600

    refactoring code
    * Change snow and doSNOW packages to parallel package
    * now the progress bar with parallel work it is with pbapply package
    * New function to crossvalidation, easier to maintain.

--- R/BMORS.R
+++ R/BMORS.R
@@ -25,15 +25,15 @@ BMORS <- function(Y = NULL, ETA = NULL, covModel = 'BRR', predictor_Sec_complete
   parallelCores <- validate.parallelCores(parallelCores)
   time.init <- proc.time()[3]
   nCV <- length(testingSet$CrossValidation_list) #Number of cross-validations
-  # Yhat_post <- matrix(NA, ncol = nCV, nrow = nrow(Y)) # save predictions
   nTraits <- dim(Y)[2L]
-  YwithCov <- matrix(Y, ncol = 2 * nTraits, nrow = nrow(Y)) # to include covariance data
+  pb <- getProgressBar(':what  [:bar]:percent;  Time elapsed: :elapsed - time left: :eta', 2L*(nCV*nTraits))
   results <- data.frame() # save cross-validation results
-  pb <- progress::progress_bar$new(format = ':what  [:bar]:percent;  Time elapsed: :elapsed - time left: :eta',
-                                   total = 2L*(nCV*nTraits), clear = FALSE, show_after = 0)
 
   if (is.null(testingSet)) {
     nNA <- 0
+
+    YwithCov <- matrix(Y, ncol = 2 * nTraits, nrow = nrow(Y)) # to include covariance data
+
     #########First stage analysis#################################
     for (t in seq_len(nTraits)) {
       if (progressBar) {
@@ -74,96 +74,78 @@ BMORS <- function(Y = NULL, ETA = NULL, covModel = 'BRR', predictor_Sec_complete
     out <- list(results = results, nIter = nIter, burnIn = burnIn, thin = thin, executionTime = proc.time()[3] - time.init, NAvalues = nNA)
     class(out) <- 'BMORS'
   } else if (parallelCores <= 1 && inherits(testingSet, 'CrossValidation')) {
+
     # Covariance
     for (actual_CV in seq_len(nCV)) {
-      #########First stage analysis#################################
-      for (t in seq_len(nTraits)) {
-        if (progressBar) {
-          pb$tick(tokens = list(what = paste0('Estimating covariates')))
-        }
-        y <- Y[, t]
-        positionTST <- testingSet$CrossValidation_list[[actual_CV]]
-        y[positionTST] <- NA
-
-        fm <- BGLR(y, ETA = ETA, nIter = nIter, burnIn = burnIn, thin = thin, verbose = FALSE)
-        YwithCov[, nTraits + t] <- fm$yHat
-      }
-
-      XPV <- scale(YwithCov[, (1L + nTraits):(2L*nTraits)])
-      ETA1 <- ETA
-      if (predictor_Sec_complete) {
-        ETA1$Cov_PreVal <- list(X = XPV, model = covModel)
-      } else {
-        ETA1 <- list(Cov_PreVal = list(X = XPV, model = covModel))
-      }
-
-      for (t in seq_len(nTraits)) {
-        if (progressBar) {
-          pb$tick(tokens = list(what = paste0('Fitting the model')))
-        }
-        y1 <- Y[, t]
-        positionTST <- testingSet$CrossValidation_list[[actual_CV]]
-        y1[positionTST] <- NA
-
-        fm <- BGLR(y1, ETA = ETA1, nIter = nIter, burnIn = burnIn, thin = thin, verbose = FALSE)
-
-        results <- rbind(results, data.frame(Position = positionTST,
-                                             Environment = testingSet$Environments[positionTST],
-                                             Trait = colnames(Y)[t],
-                                             Partition = actual_CV,
-                                             Observed = round(Y[positionTST, t], digits),
-                                             Predicted = round(fm$yHat[positionTST], digits)))
-      }
-
+      results <- rbind(results, CVBMORS(Y, testingSet, ETA, nIter, burnIn, thin, predictor_Sec_complete, covModel, digits, actual_CV, progressBar, pb))
     }
     out <- list(results = results, nIter = nIter, burnIn = burnIn, thin = thin, executionTime = proc.time()[3] - time.init)
     class(out) <- 'BMORSCV'
   } else if (parallelCores > 1 && inherits(testingSet, 'CrossValidation')) {
-    cl <- snow::makeCluster(parallelCores)
-    doSNOW::registerDoSNOW(cl)
     nCV <- length(testingSet$CrossValidation_list)
+    cl <- parallel::makeForkCluster(parallelCores)
+    pbapply::pboptions(type = ifelse(progressBar, 'timer', 'none'))
+    tmp <- pbapply::pblapply(seq_len(nCV),
+                             function(i) CVBMORS(Y, testingSet, ETA, nIter, burnIn,
+                                                 thin, predictor_Sec_complete,
+                                                 covModel, digits, i,
+                                                 progressBar, pb),
+                             cl = cl)
+    parallel::stopCluster(cl)
 
-    progress <- NULL
-    if (progressBar) {
-      pb <- utils::txtProgressBar(max = nCV, style = 3)
-      progress <- function(n) utils::setTxtProgressBar(pb, n)
-    }
-    opts <- list(progress = progress)
-    results <- foreach::foreach(actual_CV = seq_len(nCV), .combine = rbind, .packages = 'BMTME', .options.snow = opts) %dopar% {
-      for (t in seq_len(nTraits)) {
-        y <- Y[, t]
-        positionTST <- testingSet$CrossValidation_list[[actual_CV]]
-        y[positionTST] <- NA
+    res <- do.call("rbind", tmp)
 
-        fm <- BGLR(y, ETA = ETA, nIter = nIter, burnIn = burnIn, thin = thin, verbose = FALSE)
-        YwithCov[, nTraits + t] <- fm$yHat
-      }
-
-      XPV <- scale(YwithCov[, (1L + nTraits):(2L*nTraits)])
-      ETA1 <- ETA
-      ETA1$Cov_PreVal <- list(X = XPV, model = covModel)
-
-      for (t in seq_len(nTraits)) {
-        y1 <- Y[, t]
-        positionTST <- testingSet$CrossValidation_list[[actual_CV]]
-        y1[positionTST] <- NA
-
-        fm <- BGLR(y1, ETA = ETA1, nIter = nIter, burnIn = burnIn, thin = thin, verbose = FALSE)
-        results <- data.frame(Position = positionTST,
-                   Environment = testingSet$Environments[positionTST],
-                   Trait = colnames(Y)[t],
-                   Partition = actual_CV,
-                   Observed = round(Y[positionTST, t], digits),
-                   Predicted = round(fm$yHat[positionTST], digits))
-      }
-      results
-    }
-    out <- list(results = results, nIter = nIter, burnIn = burnIn, thin = thin, executionTime = proc.time()[3] - time.init)
+    out <- list(results = res, nIter = nIter, burnIn = burnIn, thin = thin, executionTime = proc.time()[3] - time.init)
     class(out) <- 'BMORSCV'
   }
 
 
   return(out)
+}
+
+CVBMORS <- function(Y, testingSet,  ETA, nIter, burnIn, thin, predictor_Sec_complete, covModel, digits, iterationNumber, progressBar, pb){
+  nTraits <- dim(Y)[2L]
+  YwithCov <- matrix(Y, ncol = 2 * nTraits, nrow = nrow(Y)) # to include covariance data
+  results <- data.frame() # save cross-validation results
+
+  for (t in seq_len(nTraits)) {
+    if (progressBar) {
+      pb$tick(tokens = list(what = paste0('Estimating covariates')))
+    }
+    y <- Y[, t]
+    positionTST <- testingSet$CrossValidation_list[[iterationNumber]]
+    y[positionTST] <- NA
+
+    fm <- BGLR(y, ETA = ETA, nIter = nIter, burnIn = burnIn, thin = thin, verbose = FALSE)
+    YwithCov[, nTraits + t] <- fm$yHat
+  }
+
+  XPV <- scale(YwithCov[, (1L + nTraits):(2L*nTraits)])
+  ETA1 <- ETA
+  if (predictor_Sec_complete) {
+    ETA1$Cov_PreVal <- list(X = XPV, model = covModel)
+  } else {
+    ETA1 <- list(Cov_PreVal = list(X = XPV, model = covModel))
+  }
+
+  for (t in seq_len(nTraits)) {
+    if (progressBar) {
+      pb$tick(tokens = list(what = paste0('Fitting the model')))
+    }
+    y1 <- Y[, t]
+    positionTST <- testingSet$CrossValidation_list[[iterationNumber]]
+    y1[positionTST] <- NA
+
+    fm <- BGLR(y1, ETA = ETA1, nIter = nIter, burnIn = burnIn, thin = thin, verbose = FALSE)
+
+    results <- rbind(results, data.frame(Position = positionTST,
+                                         Environment = testingSet$Environments[positionTST],
+                                         Trait = colnames(Y)[t],
+                                         Partition = iterationNumber,
+                                         Observed = round(Y[positionTST, t], digits),
+                                         Predicted = round(fm$yHat[positionTST], digits)))
+  }
+  return(results)
 }
 
 validate.Y <- function(matrix){
